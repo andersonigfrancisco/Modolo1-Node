ee159ebdcec7c5279559d9d3c426dda2f37edacd
andersonfranciscoig@gmail.com <andersonfranciscoig@gmail.com>
2022-07-25 13:42:22 +0100

    created method add custumer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const {v4:uuidv4} = require('uuid')
 
 const app = express();
 
 app.use(express.json())
 
+const customer = [];
+/**
+ * cpf:sting
+ * name: string
+ * id: uuid
+ * statement: []
+*/ 
 
+app.post("/account",(request,response)=>{
+
+  const {cpf,name} = request.body;
+
+  const customerAlreadyExists = customer.some((customer)=>customer.cpf ===cpf);
+
+  if(customerAlreadyExists)
+    return response.status(400).json({message:'customer already exists'})
+  
+
+  customer.push({cpf,name,id:uuidv4(),statement:[]})
+
+  return response.status(201).json({customer:customer[customer.length-1],message:'success'});
+})
 
 
 app.listen(3001);
